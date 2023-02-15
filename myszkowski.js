2bd1e030257389a7a1d206521ce6c59ec2ca2575
StringManolo <56775178+StringManolo@users.noreply.github.com>
2023-02-16 00:16:19 +0100

    Update myszkowski.js

--- myszkowski.js
+++ myszkowski.js
@@ -1,11 +1,14 @@
-function myszkowski(texto, clave) {                                                       // Eliminamos todos los espacios del texto
+function myszkowski(texto, clave) {
+  // Eliminamos todos los espacios del texto
   // texto = texto.replace(/\s/g, '');
-                                                                                          // Obtenemos la longitud de la clave y del texto                                        const longitud_clave = clave.length;
+  // Obtenemos la longitud de la clave y del texto
+  const longitud_clave = clave.length;
   const longitud_texto = texto.length;
 
   // Creamos una matriz de longitud_clave x longitud_texto
   const matriz = new Array(longitud_clave);
-  for (let i = 0; i < longitud_clave; i++) {                                                matriz[i] = new Array(longitud_texto);
+  for (let i = 0; i < longitud_clave; i++) {
+    matriz[i] = new Array(longitud_texto);
   }
 
   // Rellenamos la matriz con el texto plano
@@ -19,12 +22,13 @@ function myszkowski(texto, clave) {                                             
       }
     }
     if (indice >= longitud_texto) {
-      break;                                                                                }
+      break;
+    }
   }
 
   // Ordenamos la clave alfabéticamente
   const clave_ordenada = clave.split('').sort().join('');
-                                                                                          // Obtenemos el orden de la clave
+  // Obtenemos el orden de la clave
   const orden_clave = new Array(longitud_clave);
   for (let i = 0; i < longitud_clave; i++) {
     const letra = clave.charAt(i);
@@ -32,18 +36,21 @@ function myszkowski(texto, clave) {                                             
   }
 
   // Construimos el texto cifrado
-  let texto_cifrado = '';                                                                 for (let i = 0; i < longitud_clave; i++) {
+  let texto_cifrado = '';
+  for (let i = 0; i < longitud_clave; i++) {
     const columna = orden_clave.indexOf(i);
     for (let j = 0; j < longitud_texto; j++) {
       if (matriz[columna][j]) {
         texto_cifrado += matriz[columna][j];
       }
-    }                                                                                     }                                                                                     
+    }
+  }                                                                                     
   return texto_cifrado;
 }
 
 function desmyszkowski(texto_cifrado, clave) {
-  // Obtenemos la longitud de la clave y del texto cifrado                                const longitud_clave = clave.length;
+  // Obtenemos la longitud de la clave y del texto cifrado
+  const longitud_clave = clave.length;
   const longitud_texto_cifrado = texto_cifrado.length;
 
   // Creamos una matriz de longitud_clave x longitud_texto_cifrado
@@ -58,8 +65,10 @@ function desmyszkowski(texto_cifrado, clave) {
   const letras_adicionales = (longitud_clave * numero_columnas) - longitud_texto_cifrado;                                                                                                                                                                                 // Ordenamos la clave alfabéticamente
   const clave_ordenada = clave.split('').sort().join('');
 
-  // Obtenemos el orden de la clave                                                       const orden_clave = new Array(longitud_clave);
-  for (let i = 0; i < longitud_clave; i++) {                                                const letra = clave.charAt(i);
+  // Obtenemos el orden de la clave
+  const orden_clave = new Array(longitud_clave);
+  for (let i = 0; i < longitud_clave; i++) {
+    const letra = clave.charAt(i);
     orden_clave[i] = clave_ordenada.indexOf(letra);
   }
 
@@ -71,19 +80,23 @@ function desmyszkowski(texto_cifrado, clave) {
     if (columna >= longitud_clave - letras_adicionales) {
       letras_columna = numero_columnas - 1;
     }
-    for (let j = 0; j < letras_columna; j++) {                                                matriz[columna][j] = texto_cifrado.charAt(indice);
+    for (let j = 0; j < letras_columna; j++) {
+      matriz[columna][j] = texto_cifrado.charAt(indice);
       indice++;
     }
   }
 
   // Construimos el texto plano
-  let texto_plano = '';                                                                   for (let j = 0; j < numero_columnas; j++) {                                               for (let i = 0; i < longitud_clave; i++) {
+  let texto_plano = '';
+  for (let j = 0; j < numero_columnas; j++) {
+    for (let i = 0; i < longitud_clave; i++) {
       if (matriz[i][j]) {
         texto_plano += matriz[i][j];
       }
     }
   }                                                                                     
-  return texto_plano;                                                                   }
+  return texto_plano;
+}
 
 
 module.exports = { myszkowski, desmyszkowski };
