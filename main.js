006fde0d5bfe6858b5e6532b61108d7c56ba9744
StringManolo <56775178+StringManolo@users.noreply.github.com>
2023-02-16 00:25:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function deriveKeyFromText(textKey) {
 
 const text = "Hola me llamo manolo!";
 const key = "abc";
-                                                                                        const encrypted = myszkowski(text, key);
+const encrypted = myszkowski(text, key);
 const decrypted = desmyszkowski(encrypted, key);
 
 const encryptedVernam = vernam(text, key);
@@ -30,18 +30,23 @@ const tag = cipher.getAuthTag();
 // Preppend the tag and the IV to the text
 const concated = Buffer.concat([nonce, tag, Buffer.from(encryptedChacha)]).toString('hex');
 
-let decipher = crypto.createDecipheriv('ChaCha20-Poly1305', deriveKeyFromText(key), nonce, { authTagLength: 16 });                                                              let decryptedChacha = decipher.update(encryptedChacha, 'hex', 'utf8');
+let decipher = crypto.createDecipheriv('ChaCha20-Poly1305', deriveKeyFromText(key), nonce, { authTagLength: 16 });
+let decryptedChacha = decipher.update(encryptedChacha, 'hex', 'utf8');
 decryptedChacha += decipher.final('utf8');
-                                                                                                                                                                                const compressed = zlib.brotliCompressSync(text);
+const compressed = zlib.brotliCompressSync(text);
 const uncompressed = zlib.brotliDecompressSync(compressed).toString("utf-8");
+
 
 // This block is for debug, is not including iv and tag
 // beat stand for (Block, Encryption And Transposition)
-const beat = myszkowski(vernam(encryptedChacha, key), key);                             // reuse decipher
+const beat = myszkowski(vernam(encryptedChacha, key), key);
+// reuse decipher
 decipher = crypto.createDecipheriv('ChaCha20-Poly1305', deriveKeyFromText(key), nonce, { authTagLength: 16 });
 let revBeat = decipher.update( desvernam(desmyszkowski(beat, key), key), 'hex', 'utf-8');
 revBeat += decipher.final('utf-8');                                                     
-                                                                                        // This block is the good one, including iv and tag before encryption
+
+
+// This block is the good one, including iv and tag before encryption
 // AIBEAT stand for (Autentication, IV, Block, Encryption And Transposition)
 const aibeat = myszkowski(vernam(concated, key), key);
 let revConcated = desvernam(desmyszkowski(aibeat, key), key);
@@ -51,7 +56,8 @@ revConcated = Buffer.from(revConcated.slice(56), 'hex').toString();
 
 
 /* edit auth tag to check if it works
-// debug only                                                                           revTag = revTag.replace(/[0-9]/g, 7);
+// debug only
+revTag = revTag.replace(/[0-9]/g, 7);
 */
 
 const aibeatDecipher = crypto.createDecipheriv('ChaCha20-Poly1305', deriveKeyFromText(key), nonce, { authTagLength: 16 });
@@ -71,12 +77,14 @@ This attack has been detected using the algorithm Poly1305
 https://en.wikipedia.org/wiki/Poly1305.`);
 process.exit();
 }
-                                                                                        console.log(`                                                                           El mensaje original es: ${text}
+
+console.log(`El mensaje original es: ${text}
 La clave original es: ${key}
 
 El mensaje original comprimido es: ${compressed}
 El mensaje original descomprimido es: ${uncompressed}
-                                                                                        El mensaje original transposicionado es: ${encrypted}
+
+El mensaje original transposicionado es: ${encrypted}
 El mensaje original destransposicionado es: ${decrypted}
 
 El mensaje original encryptado con vernam es: ${encryptedVernam}
@@ -90,10 +98,12 @@ El tag de Poly1305 es: ${tag.toString('hex')}
 El mensaje original encryptado con Chacha y que incluye el tag de Poly y el IV es:
 ${concated}
 
-Chacha20-Poly1305 + Vernam + Myszkowski: ${beat}                                        Descifrado: ${revBeat}
+Chacha20-Poly1305 + Vernam + Myszkowski: ${beat}
+Descifrado: ${revBeat}
 
 Chacha20-Poly1305 + Vernam + Myszkowski (Including AuthTag and IV): ${aibeat}
 Descifrado: ${revAibeat}
-El nonce utilizado es: ${revIV.toString('hex')}                                         El tag de Poly1305 es: ${revTag}
+El nonce utilizado es: ${revIV.toString('hex')}
+El tag de Poly1305 es: ${revTag}
 
 `);
